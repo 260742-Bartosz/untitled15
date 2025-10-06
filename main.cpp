de2b4a915a8023af5415eeca9533cec37cd3cb79
Bartosz <260742@edu.p.lodz.pl>
2025-10-06 11:27:05 +0200

    02 Zmiana metody Policz

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main() {
     auto lang = "C++";
     std::cout << "Hello and welcome to " << lang << "!\n";
     std::cout << "W pierwszym branchu; 2+3=" << Policz(2,3) << std::endl;
+
     for (int i = 1; i <= 5; i++) {
         // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         std::cout << "i = " << i << std::endl;
